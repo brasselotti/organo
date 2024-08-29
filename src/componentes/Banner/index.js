1fce96ee31cedfee784e4d22e4b70e74c7fc018c
Fabiano Brasselotti <brasselottif@gmail.com>
2024-08-29 17:46:31 -0300

    first commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página do organo"/>
+        </header>
+    )
+}
+
+export default Banner
